810adddaa37dfdc4499e3f2ee093f863b3a1b336
Thomas Olaszki <olaszky@gmail.com>
2018-11-11 16:30:01 +0100

    new database

--- database_save/library.sql
+++ database_save/library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 10, 2018 at 11:08 PM
+-- Generation Time: Nov 11, 2018 at 04:29 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
